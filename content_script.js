607e88bf58f42e95071d4ed802c7bcb14a0e835a
weiz <weiz.vit@gmail.com>
2017-06-21 17:32:39 -0400

    v0.1

--- content_script.js
+++ content_script.js
@@ -1,9 +1,18 @@
+function isEditable(element) {
+    return element.localName === 'textarea'
+        || element.localName === 'select'
+        || element.isContentEditable
+        || (element.localName === 'input' && /^(?!button|checkbox|file|hidden|image|radio|reset|submit)/i.test(element.type));
+}
+
 window.addEventListener('keydown', function(event) {
     switch (event.keyCode) {
-        case 13:
-            console.log('Enter key pressed');
-            chrome.runtime.sendMessage({action: "close download shelf"});
-            event.preventDefault();
+        case 88:
+            console.log('x key pressed');
+            var elm = document.activeElement;
+            if (!isEditable(elm)) {
+              chrome.runtime.sendMessage({action: "close download shelf"});
+            }
             break;
     }
 });
